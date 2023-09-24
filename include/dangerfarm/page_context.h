d4181b10066c3be23cabb3a80987cc1cc574490a
Justin Handville <nanolith@gmail.com>
2023-09-24 18:12:45 -0400

    decl for with_page_description.

--- include/dangerfarm/page_context.h
+++ include/dangerfarm/page_context.h
@@ -66,3 +66,18 @@ int with_page_url(const char* url, callback_fn callback, FILE* out);
  *      - a non-zero error code on failure.
  */
 int with_page_site_name(const char* site_name, callback_fn callback, FILE* out);
+
+/**
+ * \brief Override the description in a page context, and call the callback with
+ * the updated page context.
+ *
+ * \param description           The description to override.
+ * \param callback              The callback to call with the context.
+ * \param out                   The output file for this callback operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int with_page_description(
+    const char* description, callback_fn callback, FILE* out);
