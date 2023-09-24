d29312acb65791826fabb7701f43ada228e4a24f
Justin Handville <nanolith@gmail.com>
2023-09-24 05:07:08 -0400

    decl for with_mkdir.

--- include/dangerfarm/directory.h
+++ include/dangerfarm/directory.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <dangerfarm/callback.h>
+
 /**
  * \brief Attempts to create the static-site directory structure.
  *
@@ -12,3 +14,18 @@
  *      - a non-zero error code on failure.
  */
 int make_site_directories();
+
+/**
+ * \brief Create the given directory, then call the given callback.
+ *
+ * \note that the out parameter will be NULL in the callback.
+ *
+ * \param dirname               The directory name to create.
+ * \param callback              The callback function to call.
+ * \param context               The context to pass to the callback.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int with_mkdir(const char dirname, callback_fn callback, void* context);
