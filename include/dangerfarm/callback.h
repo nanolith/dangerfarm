2804e135a95aaedb784f13d46e84fba1a5aac598
Justin Handville <nanolith@gmail.com>
2023-09-24 04:11:05 -0400

    decl for callback function.

---
+++ include/dangerfarm/callback.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdio.h>
+
+/**
+ * \brief The callback function is called with an opaque context and an output
+ * file.
+ *
+ * \param context           An opaque pointer to a user context item.
+ * \param out               The output file for this callback operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - non-zero error code on failure.
+ */
+typedef int (*callback_fn)(void* context, FILE* out);
