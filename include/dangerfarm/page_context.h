4b7365ad5b07efc1c3935ad197cb31db683ff42e
Justin Handville <nanolith@gmail.com>
2023-09-24 18:11:01 -0400

    Decl for with_page_site_name.

--- include/dangerfarm/page_context.h
+++ include/dangerfarm/page_context.h
@@ -52,3 +52,17 @@ int with_page_title(const char* title, callback_fn callback, FILE* out);
  *      - a non-zero error code on failure.
  */
 int with_page_url(const char* url, callback_fn callback, FILE* out);
+
+/**
+ * \brief Override the site name in a page context, and call the callback with
+ * the updated page context.
+ *
+ * \param site_name             The site name to override.
+ * \param callback              The callback to call with the context.
+ * \param out                   The output file for this callback operation.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int with_page_site_name(const char* site_name, callback_fn callback, FILE* out);
