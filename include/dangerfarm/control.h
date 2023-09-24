e139de5f13f0cb8e9880456c16dca52b3f503f04
Justin Handville <nanolith@gmail.com>
2023-09-24 04:44:28 -0400

    Added TRY_OR_FAIL variant that prints error msg.

--- include/dangerfarm/control.h
+++ include/dangerfarm/control.h
@@ -9,3 +9,14 @@
             goto fail_label; \
         } \
     } while (0)
+
+#define TRY_OR_FAIL_MSG(operation, fail_label, message) \
+    do \
+    { \
+        retval = operation; \
+        if (STATUS_SUCCESS != retval) \
+        { \
+            fprintf(stderr, "ERROR %s.\n", message); \
+            goto fail_label; \
+        } \
+    } while (0)
