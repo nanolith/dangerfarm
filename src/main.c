70ff27ed197290356a4a55dd30fca58182af2b7d
Justin Handville <nanolith@gmail.com>
2023-09-24 04:34:45 -0400

    Make directories in main.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,24 @@
+#include <dangerfarm/directory.h>
+#include <dangerfarm/status_codes.h>
 #include <stdio.h>
 
 int main(int argc, char* argv[])
 {
+    int retval;
     (void)argc;
     (void)argv;
 
-    fprintf(stderr, "Error: not yet implemented.\n");
+    printf("Creating directories...\n");
+
+    retval = make_site_directories();
+    if (STATUS_SUCCESS != retval)
+    {
+        fprintf(stderr, "Error creating directories.\n");
+        goto fail;
+    }
+
+    return 0;
+
+fail:
     return 1;
 }
