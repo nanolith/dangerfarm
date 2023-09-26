2fef97e64e5d5afe2b60a2e642570eb1579a8ae9
Justin Handville <nanolith@gmail.com>
2023-09-26 05:03:43 -0400

    decl for main_index.

---
+++ include/dangerfarm/content.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <dangerfarm/callback.h>
+
+/**
+ * \brief Create the main index page.
+ *
+ * \note This function assumes that a default page context is passed to it,
+ * along with a NULL output file.
+ *
+ * \param context           The default page context.
+ * \param out               NULL and ignored.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int main_index(page_context* context, FILE* out);
