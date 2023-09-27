a438a99be27f4ddb4ff53ef09941ab5551032d16
Justin Handville <nanolith@gmail.com>
2023-09-27 18:16:56 -0400

    Added COPY_FILE macro.

--- include/dangerfarm/content.h
+++ include/dangerfarm/content.h
@@ -16,3 +16,18 @@
  *      - a non-zero error code on failure.
  */
 int main_index(page_context* context, FILE* out);
+
+/**
+ * \brief Create the CSS file.
+ *
+ * \note This function assumes that a default page context is passed to it,
+ * along with a NULL output file.
+ *
+ * \param context           The default page context.
+ * \param out               NULL and ignored.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int css_file(page_context* context, FILE* out);
