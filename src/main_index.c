ed4aa75a37fce268facce2e79b54c1bc5d0ed6d8
Justin Handville <nanolith@gmail.com>
2023-09-27 02:00:25 -0400

    impl of main_index.

---
+++ src/main_index.c
@@ -0,0 +1,82 @@
+#include <dangerfarm/boilerplate.h>
+#include <dangerfarm/control.h>
+#include <dangerfarm/file.h>
+#include <dangerfarm/html.h>
+#include <dangerfarm/page_context.h>
+#include <dangerfarm/status_codes.h>
+#include <stdio.h>
+
+static int main_content(page_context* context, FILE* out);
+
+/**
+ * \brief Create the main index page.
+ *
+ * \note This function assumes that a default page context is passed to it,
+ * along with a NULL output file.
+ *
+ * \param context           The default page context.
+ * \param out               NULL and ignored.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int main_index(page_context* context, FILE* out)
+{
+    int retval;
+
+    WRITE_FILE(done, "static-site/index.html", {
+        PAGE_TITLE(done, "danger.farm", {
+            PAGE_URL(done, "https://danger.farm/", {
+                PAGE_SITE_NAME(done, "danger.farm", {
+                    PAGE_SITE_DESCRIPTION(done,
+                        "Danger Farm: a personal project site.", {
+                        WRAP_TEMPLATE(done, {
+                            TRY_OR_FAIL(main_content(context, out), done);
+                            XSUCCESS();
+                        });
+                        XSUCCESS();
+                    });
+                    XSUCCESS();
+                });
+                XSUCCESS();
+            });
+            XSUCCESS();
+        });
+        XSUCCESS();
+    });
+
+done:
+    return retval;
+}
+
+/**
+ * \brief The main page content.
+ *
+ * \param context           The default page context.
+ * \param out               NULL and ignored.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int main_content(page_context* context, FILE* out)
+{
+    int retval;
+
+    XH1(done, {
+        XTEXT(done, "Danger Farm");
+        XSUCCESS();
+    });
+
+    XP(done, {
+        XTEXT(done,
+            "This is my personal project website, meant for deeper dives "
+            "into projects. Really, I just like the name and thought it would "
+            "make an amusing project blog.");
+        XSUCCESS();
+    });
+
+done:
+    return retval;
+}
