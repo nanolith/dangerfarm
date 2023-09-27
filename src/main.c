3fb71bcf53417c47dc52037a3907f0a6992dc8c7
Justin Handville <nanolith@gmail.com>
2023-09-27 02:01:23 -0400

    Build the main index file.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
+#include <dangerfarm/boilerplate.h>
+#include <dangerfarm/content.h>
 #include <dangerfarm/control.h>
 #include <dangerfarm/directory.h>
+#include <dangerfarm/html.h>
+#include <dangerfarm/page_context.h>
 #include <dangerfarm/status_codes.h>
 #include <stdio.h>
 
@@ -11,6 +15,10 @@ int main(int argc, char* argv[])
 
     printf("Creating directories...\n");
     TRY_OR_FAIL_MSG(make_site_directories(), fail, "creating directories");
+
+    TRY_OR_FAIL_MSG(
+        with_default_page_context(main_index, NULL), fail,
+        "creating main index");
 
     /* success. */
     return 0;
