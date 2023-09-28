7e3f18835f043099013d965475fa243e261face9
Justin Handville <nanolith@gmail.com>
2023-09-28 01:23:44 -0400

    decl for jlink65c02_index.

--- include/dangerfarm/project_content.h
+++ include/dangerfarm/project_content.h
@@ -16,3 +16,18 @@
  *      - a non-zero error code on failure.
  */
 int project_jemu65c02_index(page_context* context, FILE* output);
+
+/**
+ * \brief Create the jlink65c02 page.
+ *
+ * \note This function assumes that a default page context is passed to it,
+ * along with a NULL output file.
+ *
+ * \param context           The default page context.
+ * \param out               NULL and ignored.
+ *
+ * \returns a status code indicating success or failure.
+ *      - STATUS_SUCCESS on success.
+ *      - a non-zero error code on failure.
+ */
+int project_jlink65c02_index(page_context* context, FILE* output);
